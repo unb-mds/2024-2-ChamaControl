1770877c5fcf4d7d334af017983d8082858c09e9
leohssjr <leonardosauma.junior@gmail.com>
2024-12-12 23:22:37 -0300

    [fix] error homepage

--- web/src/AppRoutes.jsx
+++ web/src/AppRoutes.jsx
@@ -1,5 +1,5 @@
-import { Route, Routes } from "react-router-dom";
-
+import { Route, Routes, Navigate } from "react-router-dom";
+import App from "./pages/App/App";
 import Home from './pages/Home/Home';
 import Maps from './pages/Maps/Maps';
 import Alert from './pages/Alert/Alert'; 
@@ -12,7 +12,8 @@ function AppRoutes() {
     return (
         <AuthProvider>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Navigate to="/home" />} />
+                <Route path="/home" element={<Home />} />
                 <Route path="/maps" element={<Maps />} />
                 <Route path="/alert" element={<Alert />} /> 
                 <Route path="/about" element={<About />} />
