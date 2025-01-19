40b87eb7b9e93f234fe837a2319fe13296416733
fbressa <filipebr3sa@gmail.com>
2025-01-19 16:54:30 -0300

    Arrumando o erro das imagens na build

--- web/src/layout/Navbar.jsx
+++ web/src/layout/Navbar.jsx
@@ -2,13 +2,14 @@ import { useLocation } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
 import { Link } from "react-router-dom";
+import logoPng from "../../public/assets/logo-png.png";
 
 function Navbar() {
     const location = useLocation();
 
     return (
         <div className={styles.home_header}>
-            <img src="/src/assets/logo-png.png" alt="ChamaControl" className="logo-image" />
+            <img src={logoPng} alt="ChamaControl" className="logo-image" />
             <nav className={styles.navigation}>
                 <Link to="/" className={location.pathname === "/" ? styles.active : ""}>Início</Link>
                 <Link to="/maps" className={location.pathname === "/maps" ? styles.active : ""}>Consultar Mapa</Link>
