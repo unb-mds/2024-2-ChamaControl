044b76d1ef9f454954260583c60602153432b7c4
arturhk05 <arturhandow@gmail.com>
2025-01-22 23:57:28 -0300

    feat: adiciona variavel global para definir a porta do back

--- backend/app.js
+++ backend/app.js
@@ -14,13 +14,14 @@ app.use(bodyParser.json());
 
 app.use('/api', routes);
 
-
 connection.query('SELECT 1 + 1 AS solution', function (err, rows, fields) {
 
     if (err) throw err;
     console.log('Banco de dados funcionando se = 2:', rows[0].solution);
 });
 
-app.listen(3000, () => {
-    console.log(`Servidor rodando em http://localhost:3000`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta: ${PORT}`);
 });
