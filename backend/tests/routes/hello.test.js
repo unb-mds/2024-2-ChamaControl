c5fe162b2d5437a91fd5c4b836cc9df1c754e2e4
Arturhk05 <arturhandow@gmail.com>
2025-01-24 13:41:16 -0300

    test(backend): adiciona teste simples do hello world

---
+++ backend/tests/routes/hello.test.js
@@ -0,0 +1,16 @@
+const request = require('supertest');
+const express = require('express');
+
+const app = express();
+
+app.get('/hello', (req, res) => {
+  res.status(200).send('Hello World');
+});
+
+describe('GET /hello', () => {
+  it('should return Hello World', async () => {
+    const res = await request(app).get('/hello');
+    expect(res.statusCode).toEqual(200);
+    expect(res.text).toBe('Hello World');
+  });
+});
