fc576740c4dfa82f254801fc2771f0f9f4c642f7
arturhk05 <arturhandow@gmail.com>
2025-01-25 18:31:28 -0300

    adiciona analytics do vercel no frontend

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,9 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRoutes from './AppRoutes.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/next'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <AppRoutes />
+    <Analytics />
   </BrowserRouter>,
 )
