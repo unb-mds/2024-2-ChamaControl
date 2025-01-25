527bd9214a383a66fc237c70c435960ebe6d56a1
arturhk05 <arturhandow@gmail.com>
2025-01-25 18:36:37 -0300

    resolve bug do analytics

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,11 +2,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import AppRoutes from './AppRoutes.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import { Analytics } from '@vercel/analytics/next'
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <AppRoutes />
     <Analytics />
+    <AppRoutes />
   </BrowserRouter>,
 )
