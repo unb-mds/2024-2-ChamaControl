d329f0126f76547255bc4eeeb0ff2a543295c33a
Arturhk05 <arturhandow@gmail.com>
2025-01-28 12:24:34 -0300

    fix(scraper): altera data de coleata para anterior a atual

--- backend/scraper/processaDadoDiario.js
+++ backend/scraper/processaDadoDiario.js
@@ -28,6 +28,7 @@ const OUTPUT_DIR = path.join(__dirname, 'extrato')
 
 const getCurrentDate = () => {
   const today = new Date()
+  today.setDate(today.getDate() - 1)
   return today.toISOString().split('T')[0].replace(/-/g, '')
 }
 
