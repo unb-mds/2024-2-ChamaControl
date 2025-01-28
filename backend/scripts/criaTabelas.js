f839ed18af75f697a8f365117db68463f54f0a0f
arturhk05 <arturhandow@gmail.com>
2025-01-28 20:32:58 -0300

    test(backend): adiciona DB_PORT  no teste do banco de dados

--- backend/scripts/criaTabelas.js
+++ backend/scripts/criaTabelas.js
@@ -1,15 +1,15 @@
-const fs = require('fs');
-const connection = require('../config/db.js');
+const fs = require('fs')
+const connection = require('../config/db.js')
 
-let sql = fs.readFileSync('./script-db.sql', 'utf8');
+const sql = fs.readFileSync('./script-db.sql', 'utf8')
 
-const queries = sql.split(';').filter(query => query.trim());
+const queries = sql.split(';').filter(query => query.trim())
 
 queries.forEach(query => {
   connection.query(query, (err, results) => {
-    if (err) throw err;
-    console.log('Query executada com sucesso.');
-  });
-});
+    if (err) throw err
+    console.log('Query executada com sucesso.')
+  })
+})
 
-connection.end();
+connection.end()
